7f5897bd39a531cd2801c95c4d1b0a0040c344e8
Subin Choi <50950237+dubini0@users.noreply.github.com>
2022-07-06 02:15:28 +0900

    decompile.py - Add filter

--- ghidra_auto_decompile/decompile.py
+++ ghidra_auto_decompile/decompile.py
@@ -2,6 +2,11 @@ from ghidra.app.decompiler import DecompInterface
 from ghidra.util.task import ConsoleTaskMonitor
 import sys, os
 
+linker_inserted_fns = [ '_start', 'deregister_tm_clones', 'register_tm_clones',
+  '__do_global_dtors_aux', 'frame_dummy', '__libc_csu_init', '__libc_csu_fini',
+  '_fini', '_init', '__gmon_start__']
+filter_list = ['<EXTERNAL>::', '_ITM_']
+
 # get the current program
 program = currentProgram
 decompinterface = DecompInterface()
@@ -13,6 +18,12 @@ print('[*] Binary Name : '+str(name))
 functions = program.getFunctionManager().getFunctions(True)
 
 for function in list(functions):
+    # exclude linker inserted functions
+    if any((x in str(function)) for x in filter_list):
+        continue
+    if str(function) in linker_inserted_fns:
+        continue
+        
     print("[*] Function Found : "+str(function))
     # decompile each function
     tokengrp = decompinterface.decompileFunction(function, 0, ConsoleTaskMonitor())
