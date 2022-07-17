21c69655b434dbee0dbc178de61e4ef9899044b5
Subin Choi <50950237+dubini0@users.noreply.github.com>
2022-07-17 16:23:10 +0900

    Update decompile.py

--- ghidra_auto_decompile/decompile.py
+++ ghidra_auto_decompile/decompile.py
@@ -35,19 +35,24 @@ def get_func_info(program, decompinterface, name, file_data):
         entrypoint = function.getEntryPoint()
         # c_code : <type 'unicode'>
         c_code = tokengrp.getDecompiledFunction().getC()
-        func_dict = {"funcName": str(function), "funcStartAddr": entrypoint, "decompiledFuncCode": c_code}
+        func_dict = {"funcName": str(function), "funcStartAddr": str(entrypoint), "decompiledFuncCode": str(c_code)}
         file_data["funcInfo"].append(func_dict)
         
     return file_data
 
-def dict_to_json(file_data):
-    # TODO
-    print("TODO")
+def dict_to_txt(name, file_data):
+    #print(json.dumps(file_data, ensure_ascii=False, indent='\t'))
+    filename = './results/'+name+'.txt'
+    with open(filename, 'w') as f:
+        #json.dump(file_data, f, ensure_ascii=False)
+        f.write(str(file_data))
+    print("[*] Done writing in "+filename)
 
 if __name__ == "__main__":
     info = init()
     program, decompinterface, name = info[0], info[1], info[2]
     file_data = {"isStripped": "True", "decompilerName": "Ghidra", "compilerName": "gcc", "optLevel": "-O", "binaryName": str(name)}
+    
     file_data = get_func_info(program, decompinterface, name, file_data)
-    print(file_data)
-    dict_to_json(file_data)
+    #print(file_data)
+    dict_to_txt(str(name), file_data)
